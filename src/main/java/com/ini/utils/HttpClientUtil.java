fa98cd6336de71dc6871c69f262e419965f945dd
yjdini <yjdini@163.com>
2017-07-12 00:12:09 +0800

    asdf

---
+++ src/main/java/com/ini/utils/HttpClientUtil.java
@@ -0,0 +1,12 @@
+package com.ini.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+public class HttpClientUtil {
+}
+
+
